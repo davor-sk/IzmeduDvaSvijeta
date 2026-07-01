b7d5036ee10498b68e12b4f7c76226f415ea388c
Davor <davor.skiljic@gmail.com>
2026-07-01 20:37:26 +0200

    Novi layout implementiran

--- Assets/Scripts/DhornPresenter.cs
+++ Assets/Scripts/DhornPresenter.cs
@@ -1,11 +1,13 @@
 using System.Threading;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using TMPro;
 using Yarn.Unity;
 
 public class DhornPresenter : DialoguePresenterBase
 {
-    public TextMeshProUGUI messageText;
+    public TextMeshProUGUI speakerNameText;
+    public TextMeshProUGUI dialogueText;
 
     public override YarnTask OnDialogueStartedAsync()
     {
@@ -19,12 +21,30 @@ public class DhornPresenter : DialoguePresenterBase
 
     public override async YarnTask RunLineAsync(LocalizedLine line, LineCancellationToken token)
     {
-        messageText.text = line.TextWithoutCharacterName.Text;
-        await YarnTask.WaitUntilCanceled(token.NextContentToken);
+        speakerNameText.text = line.CharacterName ?? "";
+        dialogueText.text = line.TextWithoutCharacterName.Text;
+
+        bool advanced = false;
+        while (!advanced)
+        {
+            if (Keyboard.current != null && (
+                Keyboard.current.enterKey.wasPressedThisFrame ||
+                Keyboard.current.spaceKey.wasPressedThisFrame))
+            {
+                advanced = true;
+            }
+            else if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
+            {
+                advanced = true;
+            }
+            await YarnTask.Yield();
+        }
     }
 
     [System.Obsolete]
-    public override YarnTask<DialogueOption> RunOptionsAsync(DialogueOption[] dialogueOptions, CancellationToken cancellationToken)
+    public override YarnTask<DialogueOption> RunOptionsAsync(
+        DialogueOption[] dialogueOptions,
+        CancellationToken cancellationToken)
     {
         return YarnTask<DialogueOption>.FromResult(null);
     }
